3b6cf405f75eff18618a8bb55edcca0867d0774c
Luis Leon <lleon95@gmail.com>
2022-10-29 17:45:59 -0600

    Add working prototype of the server

--- server.py
+++ server.py
@@ -17,7 +17,11 @@ pyaudio_iface = pyaudio.PyAudio()  # Create an interface to PortAudio
 stream_mic = None
 
 SAMPLE_RATE = 8000
-WINDOW_SIZE = 1024
+WINDOW_SIZE = int(SAMPLE_RATE / 4)
+CURR_PASS = "1234"
+
+# Adjust according to the volume
+threshold = 8000
 
 def goertzel(samples, sample_rate, *freqs):
     """
@@ -89,28 +93,9 @@ def text_to_speech(text, delay=5):
     while(mixer.get_busy()):
         continue
 
-def welcome_message():
-    welcome = "Bienvenido al buzón de voz de Kolbi"
-    text_to_speech(welcome, 3)
-
-def password_request():
-    message = "Digite la contraseña para acceder al buzón de voz"
-    text_to_speech(message)
-
 def number_to_speech(num):
     char = str(num)
     text_to_speech(char, 0.75)
-
-def plot_frequencies(samples, freqs, results):
-    plt.subplot(2, 1, 1)
-    plt.title('(1) Sine wave 440Hz + 1020Hz')
-    plt.plot(samples)
-
-    plt.subplot(2, 1, 2)
-    plt.title('(1) Goertzel Algo, freqency ranges : [400, 500] and [1000, 1100]')
-    plt.plot(freqs, np.array(results)[:,2], 'o')
-    plt.ylim([0,100000])
-    plt.draw()
 
 def analyse_sample(sample):
     global SAMPLE_RATE
@@ -125,47 +110,124 @@ def open_mic():
     channels = 1
     a_format = pyaudio.paInt16
     
+    # WE NEED TO CLOSE THE MIC
     stream_mic = pyaudio_iface.open(format=a_format,
             channels=channels,
             rate=SAMPLE_RATE,
             frames_per_buffer=WINDOW_SIZE,
             input=True)
 
+def close_mic():
+    global stream_mic
+    stream_mic.stop_stream()
+    stream_mic.close()
 
 def get_sample():
-    global stream_mic
     global WINDOW_SIZE
     divider = 1. / float(1 << 15)
     inputdata = stream_mic.read(WINDOW_SIZE)
     inputsample = np.frombuffer(inputdata, dtype=np.int16)
     sample = inputsample.astype(np.float32) * divider
+
     return sample
 
-if __name__ == "__main__":
-    th = 10000
-    #welcome_message()
-    #password_request()
-    #number_to_speech(1)
-    #number_to_speech(2)
+def get_row(freqs):
+    row_freq = freqs[0]
+    if row_freq < 750:
+        return 0
+    elif row_freq < 820:
+        return 1
+    elif row_freq < 900:
+        return 2
+    elif row_freq < 1000:
+        return 3
+    else:
+        return None
 
-    # Start capturing
+def get_col(freqs):
+    col_freq = freqs[len(freqs) - 1]
+    if col_freq < 1100:
+        return None
+    if col_freq < 1300:
+        return 0
+    elif col_freq < 1400:
+        return 1
+    elif col_freq < 1500:
+        return 2
+    else:
+        return 3
+
+def get_digit(row, col):
+    digits = '123A456B789C*0#D'
+    return digits[row * 4 + col]
+
+def tell_password(password):
+    message = "La contraseña ingresada es"
+    text_to_speech(message)
+    for i in password:
+        text_to_speech(i, delay=0.8)
+
+def get_input(expected=1):
+    global threshold
     open_mic()
-    plt.show()
-    while (True):
+    print("Listening...")
+    digited = ""
+    while (len(digited) < expected):
         sample = get_sample()
         freqs, results = analyse_sample(sample)
         processed = np.array(results)[:,2]
-        #plot_frequencies(sample, freqs, results)
-        
+
         maxval = processed.max()
-        if (maxval > th):
-            print("Freqs")
-            for i in freqs:
-                print(i)
-            print("Procs")
-            for i in processed:
-                print(i)
-            
+        if (maxval > threshold):
+            m = np.ma.masked_where(processed < threshold, freqs)
+            read_freqs = m.compressed()
+
+            row = get_row(read_freqs)
+            col = get_col(read_freqs)
+
+            if row is not None and col is not None:
+                digit = get_digit(row, col)
+                digited += digit
+                print(digit)
+    close_mic()
+    return digited
+
+
+if __name__ == "__main__":
+    text_to_speech("Bienvenido al buzón de voz de Kolbi", 3)
+    text_to_speech("Digite la contraseña para acceder al buzón de voz", 5)
+
+
+    while(True):
+        password = get_input(4)
+        if password != CURR_PASS:
+            text_to_speech("Contraseña incorrecta. Digite nuevamente", delay=5)    
         else:
-            print("NS")
+            text_to_speech("Bienvenido", delay=2)
+            break
     
+    while(True):
+        text_to_speech("Digite 0 si desea escuchar mensajes. 1 para cambiar la contraseña", delay=10)
+        digited = get_input(1)
+        if digited == '0':
+            text_to_speech("No tiene mensajes nuevos. Hasta luego")
+            exit(0)
+        elif digited == '1':
+            break
+        else:
+            text_to_speech("Opción inválida")
+    
+    while(True):
+        text_to_speech("Digite la nueva contraseña")
+        
+        password = get_input(4)
+        tell_password(password)
+
+        text_to_speech("Digite 1 si es correcto. Otro para repetir")
+        digited = get_input(1)
+
+        if digited == '1':
+            text_to_speech("Gracias. Hasta luego")
+            break
+        else:
+            text_to_speech("Opción inválida")
