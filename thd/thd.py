c6c03a79e30bd033eb406645279f888ddb72d75f
Allan Navarro <anavarro3106@gmail.com>
2022-11-06 15:10:13 -0600

    Plot fft

--- thd/thd.py
+++ thd/thd.py
@@ -9,29 +9,38 @@ from math import pi
 
 from matplotlib import pyplot as plt
 from scipy.io import wavfile
+freqs = [1209, 1336, 1477, 1633,697, 770, 852, 941]
 
 
-Fs, samples = wavfile.read("1336.wav")
+fig, axes = plt.subplots(nrows=int(np.ceil(len(freqs)/2)), ncols=2, figsize=(12, 8))
 
-X = fftpack.fft(samples)
-fs = fftpack.fftfreq(len(samples)) * Fs
-X = np.abs(X)
+counter = 0
+for F in freqs:
+    Fs, samples = wavfile.read(f'{F}.wav')
 
-X2 = X[len(X)//2:]
-argmax = np.argmax(X2)
-X2 =X2[argmax:]
+    X = fftpack.fft(samples)
+    fs = fftpack.fftfreq(len(samples)) * Fs
+    X = np.abs(X)
 
-thd = np.sqrt(np.sum(X2[2:]**2))/X2[0]*100
+    X2 = X[len(X)//2:]
+    argmax = np.argmax(X2)
+    X2 =X2[argmax:]
 
-print(f'THD {thd:.2f}%')
+    thd = np.sqrt(np.sum(X2[2:]**2))/X2[0]*100
 
-fig, ax = plt.subplots()
+    print(f'THD {thd:.2f}%')
 
-ax.stem(fs, X)
-ax.set_xlabel('Frequency in Hertz [Hz]')
-ax.set_ylabel('Frequency Domain (Spectrum) Magnitude')
-ax.set_xlim(-Fs / 2, Fs / 2)
+    row = counter//2
+    col = counter % 2
 
+    axes[row,col].set_title(f'{F} Hz')
+    axes[row,col].stem(fs, X)
+    axes[row,col].set_xlabel('Frequency [Hz]')
+    axes[row,col].set_ylabel('Spectrum Magnitude')
+    axes[row,col].set_xlim(-Fs / 2, Fs / 2)
+    counter += 1
+
+fig.tight_layout()
 plt.show()
 
 
